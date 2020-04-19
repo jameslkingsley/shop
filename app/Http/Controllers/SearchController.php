067294217cb106bcdc99816cab8b1f1b1c9fd464
jameslkingsley <james.kingsley@routenote.com>
2020-04-19 21:54:33 +0100

    WIP

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,14 +13,18 @@ class SearchController extends Controller
 
         return DB::connection('sle')->select('
             SELECT prodID AS id, prodRef as ref, prodTitle AS title, prodUnitSize AS unit_size, CAST((prodOurPrice * 100) AS SIGNED) AS price
-            FROM tblProducts
+            FROM tblproducts
+            INNER JOIN tblProductCats ON prodCatID = pcatID
+            INNER JOIN tblProductGroups ON pcatGroup = pgID
             WHERE prodStatus = "active"
-            AND prodLastBought > DATE_ADD(NOW(), INTERVAL -365 DAY)
             AND prodOurPrice != 0
             AND (
                 prodTitle LIKE "%' . $request->keyword . '%"
+                OR pcatTitle LIKE "%' . $request->keyword . '%"
                 OR prodID = CONVERT("' . $request->keyword . '", SIGNED INTEGER)
             )
+            AND pcatShow = 1
+            AND pgShow = 1
             ORDER BY prodTitle, prodUnitSize, prodID
             LIMIT ' . $request->query('limit', 50) . '
         ');
